e988480d6a49e2604d42379c9788cf09d141b08a
peugeot <thomaspeugeot@yahoo.fr>
2022-10-09 12:06:01 +0200

    added refresh

--- ng/projects/gongmattreespecific/src/lib/tree/tree.component.ts
+++ ng/projects/gongmattreespecific/src/lib/tree/tree.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+
+import * as gongmattree from 'gongmattree'
 
 @Component({
   selector: 'lib-tree',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeComponent implements OnInit {
 
-  constructor() { }
+
+  public gongmattreeFrontRepo?: gongmattree.FrontRepo
+
+  constructor(
+    private gongmattreeFrontRepoService: gongmattree.FrontRepoService,
+    private gongmattreeCommitNbService: gongmattree.CommitNbService,
+    private gongmattreePushFromFrontNbService: gongmattree.PushFromFrontNbService,
+
+  ) { }
+
+
+  // the component is refreshed when modification are performed in the back repo 
+  // 
+  // the checkCommitNbTimer polls the commit number of the back repo
+  // if the commit number has increased, it pulls the front repo and redraw the diagram
+
+  checkCommitNbTimer: Observable<number> = timer(500, 500);
+  lastCommitNb = -1
+  lastPushFromFrontNb = -1
+  currTime: number = 0
 
   ngOnInit(): void {
+
+    this.checkCommitNbTimer.subscribe(
+      currTime => {
+        this.currTime = currTime
+
+        // see above for the explanation
+        this.gongmattreeCommitNbService.getCommitNb().subscribe(
+          commitNb => {
+            if (this.lastCommitNb < commitNb) {
+
+              console.log("last commit nb " + this.lastCommitNb + " new: " + commitNb)
+              // this.refresh()
+              this.lastCommitNb = commitNb
+            }
+          }
+        )
+
+        // see above for the explanation
+        this.gongmattreePushFromFrontNbService.getPushFromFrontNb().subscribe(
+          pushFromFrontNb => {
+            if (this.lastPushFromFrontNb < pushFromFrontNb) {
+
+              console.log("last commit nb " + this.lastPushFromFrontNb + " new: " + pushFromFrontNb)
+              // this.refresh()
+              this.lastPushFromFrontNb = pushFromFrontNb
+            }
+          }
+        )
+      }
+    )
+  }
+
+  refresh(): void {
+
+    this.gongmattreeFrontRepoService.pull().subscribe(
+      gongmattreesFrontRepo => {
+        this.gongmattreeFrontRepo = gongmattreesFrontRepo
+
+      }
+
+    )
   }
 
 }
